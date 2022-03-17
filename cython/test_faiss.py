f7b97a3c8e98642137a9c856396e6e725fbdaf87
mrjakobdk <Vebl8550>
2022-03-17 08:43:29 +0100

    .. changed from mnist to fashion mnist

--- cython/test_faiss.py
+++ cython/test_faiss.py
@@ -3,18 +3,19 @@ import torch
 import torchvision.datasets as datasets
 import time
 
-mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+mnist_trainset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=None)
 print(mnist_trainset.train_data.shape)
 D = mnist_trainset.train_data.view(mnist_trainset.train_data.shape[0], -1).to(torch.float32)
 print(D.shape)
 D = D.numpy()
+q = D
 
 k = 20
 
 res = faiss.StandardGpuResources()
 
 t0 = time.time()
-dist, N = faiss.knn_gpu(res, D, D, k)
+dist, N = faiss.knn_gpu(res, q, D, k)
 t1 = time.time()
 
 print("time", t1 - t0)
