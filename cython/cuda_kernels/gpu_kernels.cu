18b38f420188ae31959d995acbac4fa12f7fe18b
mrjakobdk <Vebl8550>
2022-05-25 11:34:55 +0200

    tried changing number_of_blocks_scalar

--- cython/cuda_kernels/gpu_kernels.cu
+++ cython/cuda_kernels/gpu_kernels.cu
@@ -431,7 +431,7 @@ void gpu_umap_full_N(int normalized, // unused
                      int negative_sample_rate
 ) {
     cudaDeviceSynchronize();
-    int number_of_blocks_scalar = 64;//32 can be replace with something smaller then BLOCK_SIZE
+    int number_of_blocks_scalar = 32;//32 can be replace with something smaller then BLOCK_SIZE
     int number_of_threads_in_total = BLOCK_SIZE * 2 * number_of_blocks_scalar;
 
     //allocated and copy memory to the gpu
