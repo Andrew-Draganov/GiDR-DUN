b41b076fc3fdf8dac04e1bf5fa07629381201fb7
Andrew <draganovandrew@gmail.com>
2022-07-12 12:52:47 +0000

    Changed paths around

--- GDR/tests/unit_tests/test_graph_weights.py
+++ GDR/tests/unit_tests/test_graph_weights.py
@@ -15,23 +15,24 @@ class HyperParamTest(unittest.TestCase):
         self.points, _ = load_fake_data()
         self.model = get_algorithm('gdr', TEST_PARAMS)
         self.model.get_nearest_neighbors(self.points)
+
         self.dists = self.model._knn_dists
         self.inds = self.model._knn_indices.astype(np.int64)
-
         self.sigmas, self.rhos = get_sigmas_and_rhos(self.dists, 15)
+        self.values_path = os.path.join('GDR', 'tests', 'utils', 'unit_test_values')
 
     def test_neighbors(self):
         knn_dists = self.model._knn_dists
         knn_indices = self.model._knn_indices
 
-        correct_val_path = os.path.join('GDR', 'tests', 'utils', 'knn.npy')
-        correct_values = np.load(correct_val_path, allow_pickle=True)[()]
+        neighbor_val_path = os.path.join(self.values_path, 'knn.npy')
+        correct_values = np.load(neighbor_val_path, allow_pickle=True)[()]
         np.testing.assert_allclose(knn_dists, correct_values['dists'])
         np.testing.assert_allclose(knn_indices, correct_values['inds'])
 
     def test_sigmas_rhos(self):
-        correct_val_path = os.path.join('GDR', 'tests', 'utils', 'sigmas_rhos.npy')
-        correct_values = np.load(correct_val_path, allow_pickle=True)[()]
+        sigma_rho_path = os.path.join(self.values_path, 'sigmas_rhos.npy')
+        correct_values = np.load(sigma_rho_path, allow_pickle=True)[()]
         np.testing.assert_allclose(self.sigmas, correct_values['sigmas'])
         np.testing.assert_allclose(self.rhos, correct_values['rhos'])
 
@@ -43,8 +44,8 @@ class HyperParamTest(unittest.TestCase):
             self.rhos,
             pseudo_distance=True
         )
-        correct_val_path = os.path.join('GDR', 'tests', 'utils', 'pseudo_dist_similarities.npy')
-        correct_values = np.load(correct_val_path, allow_pickle=True)[()]
+        sim_value_path = os.path.join(self.values_path, 'pseudo_dist_similarities.npy')
+        correct_values = np.load(sim_value_path, allow_pickle=True)[()]
         np.testing.assert_allclose(rows, correct_values['rows'])
         np.testing.assert_allclose(cols, correct_values['cols'])
         np.testing.assert_allclose(vals, correct_values['vals'])
@@ -57,8 +58,8 @@ class HyperParamTest(unittest.TestCase):
             self.rhos,
             pseudo_distance=False
         )
-        correct_val_path = os.path.join('GDR', 'tests', 'utils', 'normal_dist_similarities.npy')
-        correct_values = np.load(correct_val_path, allow_pickle=True)[()]
+        sim_value_path = os.path.join(self.values_path, 'normal_dist_similarities.npy')
+        correct_values = np.load(sim_value_path, allow_pickle=True)[()]
         np.testing.assert_allclose(rows, correct_values['rows'])
         np.testing.assert_allclose(cols, correct_values['cols'])
         np.testing.assert_allclose(vals, correct_values['vals'])
--- GDR/tests/utils/testing_data.py
+++ GDR/tests/utils/testing_data.py
@@ -5,4 +5,3 @@ def load_fake_data(dim=10, num_points=100, num_classes=10, seed=12345):
     points = np.random.multivariate_normal(np.zeros([dim]), np.eye(dim), size=num_points)
     labels = np.random.choice(10, size=num_points, replace=True)
     return points, labels
-
