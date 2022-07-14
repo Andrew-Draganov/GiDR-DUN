de897cd4579c4d9e21b36a8293e3df175c59b02d
Andrew Alexander Draganov <draganovandrew@cs.au.dk>
2022-07-14 10:05:16 +0200

    Updated data download scripts to go to the right directory

--- GDR/scripts/cifar_get_data.sh
+++ GDR/scripts/cifar_get_data.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #from https://github.com/sorki/python-mnist/blob/master/bin/mnist_get_data.sh
 
-if [ -d GiDR-DUN/data/cifar ]; then
+if [ -d GDR/data/cifar ]; then
     echo "data directory already present, exiting"
     exit 1
 fi
 
-mkdir -p GiDR-DUN/data/cifar
-wget --directory-prefix=GiDR-DUN/data/cifar https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-pushd GiDR-DUN/data/cifar
+mkdir -p GDR/data/cifar
+wget --directory-prefix=GDR/data/cifar https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+pushd GDR/data/cifar
 tar -xvzf cifar-10-python.tar.gz
 mv cifar-10-batches-py/* .
 rm -r cifar-10-batches-py
